f6675a41e05c43afca5a7ec4b4c53c3741252d7f
Yevhen Udovychenko <yevudo@gmail.com>
2026-03-21 16:07:11 +0200

    RB-008 widget urls updated

--- app/components/btn-book-now.vue
+++ app/components/btn-book-now.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
   const { locale } = useI18n();
+
+  const widgetUrl = computed(() =>
+    locale.value === 'uk' ? CONSTANTS.widgetUrlUk : CONSTANTS.widgetUrlEn
+  );
 </script>
 
 <template>
   <a
+    :key="locale"
     href="#"
-    :data-url="locale === 'uk' ? CONSTANTS.widgetUrlUk : CONSTANTS.widgetUrlEn"
+    :data-url="widgetUrl"
     class="ms_booking mx-auto mb-12 flex h-12 w-max cursor-pointer items-center justify-center bg-gold px-8 text-ink-black"
   >
     <span><slot /></span>
